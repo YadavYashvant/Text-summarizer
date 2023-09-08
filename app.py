48e903063924314cbf5dab0ae6bd8efe677c7f50
YadavYashvant <yashvantyadav855@gmail.com>
2023-09-09 00:30:46 +0530

    adding python code

--- app.py
+++ app.py
@@ -3,3 +3,20 @@ from flask import Flask ,render_template,request
 from summarizer import Summarizer
 
 from summarizer.sbert import SBertSummarizer
+
+model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
+
+app = Flask(__name__)
+
+@app.route("/")
+def message():
+    return render_template('/templates/index.html')
+
+@app.route("/summarize", methods=['POST','GET'])
+def getSummary():
+    body = request.form['data']
+    result = model(body, num_sentences=5)
+    return render_template('/templates/summary.html', result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
